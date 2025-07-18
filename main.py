5dc222e530549b65aa59f7d66d63174ecc63d42f
nikotpbn <niko@Nicolaus-MacBook-Pro.local>
2025-07-18 10:53:25 +0100

    multiple and required params

--- main.py
+++ main.py
@@ -2,7 +2,42 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# http://127.0.0.1:8000/items/foo
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+# http://127.0.0.1:8000/items/?skip=0&limit=10
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+# http://127.0.0.1:8000/1/foo
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# http://127.0.0.1:8000/items/foo-item
+# http://127.0.0.1:8000/items/foo-item?needy=sooooneedy
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
